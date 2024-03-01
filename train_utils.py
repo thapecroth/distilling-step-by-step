38448c516846605667320537c553b0ae744492f6
Enhao Zhang <enhaoz@cs.washington.edu>
2024-02-29 19:18:52 -0800

    add lora training

--- train_utils.py
+++ train_utils.py
@@ -21,6 +21,7 @@ from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer
 from transformers import T5ForConditionalGeneration
 from transformers import DataCollatorForSeq2Seq
 from transformers.trainer_utils import set_seed
+from peft import get_peft_config, get_peft_model, LoraConfig, TaskType
 
 from model_utils import TaskPrefixDataCollator, TaskPrefixTrainer
 
@@ -36,7 +37,7 @@ def train_and_evaluate(args, run, tokenizer, tokenized_datasets, compute_metrics
 
     if args.parallelize:
         model.parallelize()
-    
+
     config_dir = get_config_dir(args)
     output_dir = f'ckpts/{config_dir}/{run}'  # for model ckpts
     logging_dir = f'logs/{config_dir}/{run}'  # for training logs
@@ -94,7 +95,7 @@ def train_and_evaluate(args, run, tokenizer, tokenized_datasets, compute_metrics
         'tokenizer': tokenizer,
         'compute_metrics': compute_metrics,
     }
-    
+
 
     if args.model_type == 'task_prefix':
         trainer = TaskPrefixTrainer(**trainer_kwargs)
@@ -104,6 +105,93 @@ def train_and_evaluate(args, run, tokenizer, tokenized_datasets, compute_metrics
         trainer = Seq2SeqTrainer(**trainer_kwargs)
     else:
         raise ValueError
-    
+
+
+    trainer.train()
+
+def lora_train_and_evaluate(args, run, tokenizer, tokenized_datasets, compute_metrics):
+
+    peft_config = LoraConfig(
+        task_type=TaskType.SEQ_2_SEQ_LM,
+        inference_mode=False,
+        r=8,
+        lora_alpha=32,
+        lora_dropout=0.1
+    )
+    model = T5ForConditionalGeneration.from_pretrained(args.from_pretrained)
+    model = get_peft_model(model, peft_config)
+    model.print_trainable_parameters()
+
+    if args.parallelize:
+        model.parallelize()
+
+    config_dir = get_config_dir(args)
+    output_dir = f'ckpts/{config_dir}/{run}'  # for model ckpts
+    logging_dir = f'logs/{config_dir}/{run}'  # for training logs
+
+    if args.no_log:
+        logging_strategy = 'no'
+        logging_dir = None
+    else:
+        logging_strategy = 'steps'
+
+    # clear output dir if already exists
+    if os.path.exists(output_dir):
+        logging.info('Found existing ckpt directory. Deleted the old directory for the latest run.')
+        shutil.rmtree(output_dir)
+
+    training_args = Seq2SeqTrainingArguments(
+        output_dir,
+        remove_unused_columns = False,
+        evaluation_strategy = 'steps',
+        eval_steps=args.eval_steps,
+        save_strategy='no',
+        save_steps=args.eval_steps,
+        logging_dir=logging_dir,
+        logging_strategy=logging_strategy,
+        logging_steps=args.eval_steps,
+        max_steps=args.max_steps,
+        learning_rate=args.lr,
+        gradient_accumulation_steps=args.grad_steps,
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size,
+        predict_with_generate=True,
+        seed=run,
+        local_rank=args.local_rank,
+        bf16=args.bf16,
+        generation_max_length=args.gen_max_len,
+        prediction_loss_only=False,
+    )
+
+    if args.model_type == 'task_prefix':
+        data_collator = TaskPrefixDataCollator(tokenizer=tokenizer, model=model)
+    elif args.model_type == 'standard':
+        data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
+    else:
+        raise ValueError
+
+
+    trainer_kwargs = {
+        'alpha': args.alpha,
+        'output_rationale': args.output_rationale,
+        'model': model,
+        'args': training_args,
+        'train_dataset': tokenized_datasets["train"],
+        'eval_dataset': {'test': tokenized_datasets["test"],},
+        'data_collator': data_collator,
+        'tokenizer': tokenizer,
+        'compute_metrics': compute_metrics,
+    }
+
+
+    if args.model_type == 'task_prefix':
+        trainer = TaskPrefixTrainer(**trainer_kwargs)
+    elif args.model_type == 'standard':
+        trainer_kwargs.pop('alpha')
+        trainer_kwargs.pop('output_rationale')
+        trainer = Seq2SeqTrainer(**trainer_kwargs)
+    else:
+        raise ValueError
+
 
     trainer.train()
